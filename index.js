2602ed32f86c4d1226dc77d07e3b52c04ecc6a6b
goldcontainer <joshuabkim95@gmail.com>
2021-06-09 16:27:46 -0400

    heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -10,6 +11,8 @@ morgan.token('person', (request) => {
 })
 
 app.use(morgan('tiny'))
+app.use(express.static('build'))
+app.use(cors())
 
 let persons = [
 	{
@@ -95,7 +98,7 @@ app.delete('/api/persons/:id', (request, response) => {
 	response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
