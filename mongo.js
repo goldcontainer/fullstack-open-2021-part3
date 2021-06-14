fe4bc0adfe65845e138edf116d931140af2cba17
goldcontainer <joshuabkim95@gmail.com>
2021-06-14 12:19:57 -0400

    fixed undefined entries

--- mongo.js
+++ mongo.js
@@ -10,35 +10,41 @@ const password = process.argv[2]
 const url =
   `mongodb+srv://fullstack:${password}@cluster0.byyjv.mongodb.net/phonebook-app?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 })
 
-const Person = mongoose.model('Person', personSchema, 'persons')
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
   let count = 0;
-  Person.find({}).then((result) => {
+  Person
+  	.find({})
+  	.then((result) => {
     console.log("phonebook:\n");
     result.forEach((person) => {
       console.log(`${person.name} ${person.number}`);
       count += 1;
     });
-    mongoose.connection.close();
     console.log("\ntotal:", count);
+    mongoose.connection.close();
     process.exit(1);
   });
 }
 
-const person = new Person({
-  name: process.argv[3],
-  number: process.argv[4],
-})
+if (process.argv.length === 5) {
+	const person = new Person({
+	  name: process.argv[3],
+	  number: process.argv[4],
+	})
 
-person.save().then(result => {
-  console.log(`added ${result.name} number ${result.number} to phonebook`)
-  mongoose.connection.close()
-})
+	person.save().then(result => {
+	  console.log(`added ${result.name} number ${result.number} to phonebook`)
+	  mongoose.connection.close()
+	}).catch((error) => {
+		console.log(error)
+	})
+}
